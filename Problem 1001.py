ce9887810aba26507b488457736d95583535756d
SS COMPUTER <jahidjesan508@gmail.com>
2023-03-13 18:51:32 +0600

    update

---
+++ Problem 1001.py
@@ -0,0 +1,9 @@
+'''
+Read 2 variables, named A and B and make the sum of these two variables,
+ assigning its result to the variable X. Print X as shown below.
+Print endline after the result otherwise you will get “Presentation Error”.
+'''
+A = int(input())  # enter this numbers
+B = int(input())  # enter this number (keyboard)
+sum = A + B  # sum 2 input numbers
+print("X =", sum)  # print this output
