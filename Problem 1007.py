6b54b13d8d53dfd46ee9ff1694f87df96074ca9c
SS COMPUTER <jahidjesan508@gmail.com>
2023-03-20 20:49:15 +0600

    new problem solve this

---
+++ Problem 1007.py
@@ -0,0 +1,21 @@
+'''
+Read four integer values named A, B, C and D. Calculate and print the difference of product
+A and B by the product of C and D (A * B - C * D).
+
+Input
+The input file contains 4 integer values.
+
+Output
+Print DIFERENCA (DIFFERENCE in Portuguese) with all the capital letters, according to the following example,
+with a blank space before and after the equal signal.
+'''
+
+# input this forth integer numbers ?
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+# equation solve
+Solve = (A * B - C * D)
+print(f"DIFERENCA = {Solve}")  # output this
+
