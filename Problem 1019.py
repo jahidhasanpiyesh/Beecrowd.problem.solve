24c0fa4026b6aa4d92acba8e2c0d179914d0f7e4
SS COMPUTER <jahidjesan508@gmail.com>
2023-05-04 11:50:38 +0600

    new problem solve this 1019

--- Problem 1019.py
+++ Problem 1019.py
@@ -1,8 +1,12 @@
 # solve this bee 1019 problem solve :
-seconds = int(input())
-minutes = int(seconds/60)
-seconds -= minutes*60
-hours = int(minutes/60)
-minutes -= hours*60
-print(f'{hours}:{minutes}:{seconds}')
-
+# input this int numbers
+second = int(input())
+# second / minutes and int type data not a float data ?
+minutes = int(second/60)
+# modulus use this float answer not allow so point break
+second %= 60
+# minutes / 60 and int type data access
+hor = int(minutes/60)
+minutes %= 60
+# use this short way and one line output
+print(f"{hor}:{minutes}:{second}")
